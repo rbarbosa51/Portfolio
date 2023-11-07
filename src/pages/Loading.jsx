141c0de00c5c36da83af66756715e9e2a4ad9ca1
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-11-06 22:08:24 -0600

    Modified Loading Page

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -3,6 +3,7 @@ import {Text, Html, useProgress} from '@react-three/drei'
 export default function Loading() {
     const {progress} = useProgress()
     return (
-            <Html center>{progress} % loaded</Html>
+            // <Html center>{progress} % loaded</Html>
+            <Text>{progress.toFixed(2)}% Loaded</Text>
     )
 }
