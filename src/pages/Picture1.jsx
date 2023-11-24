17a00c667d2486c1660c6708e0d4de2881247d8f
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-11-24 12:14:43 -0600

    Added a Button to Picture1

--- src/pages/Picture1.jsx
+++ src/pages/Picture1.jsx
@@ -11,21 +11,9 @@ export default function Picture1() {
         <ambientLight intensity={1.0} />
         <Environment preset="sunset" />
         <Sky distance={50} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25}  />
-        {/* <OrbitControls enableZoom={true} enablePan={false} enableRotate={false} maxZoom={1.1} minZoom={0.9}/> */}
-        
-          {/* <Model /> */}
-          {/* <Rafael position={[-1.5,-1,3]} rotation={[0,Math.PI * 0.25,0]}/> */}
-          {/* <Rafael position={[-1.5,-1,3]} rotation={[0,0.261799,0]}/> */}
-          <Rafael position={[0,-1,3.2]}/>
-          <Html as="div" >
-            {/* <div className="absolute   left-1/2 -translate-x-1/2"> */}
-                <button className="neonText  w-16 lg:w-32 rounded-xl bg-primary px-6 py-2.5 text-sm text-white hover:bg-primary/80">Go Back</button>
-            {/* </div> */}
-
-            
-          </Html>
-        
+        <Rafael position={[0,-1,3.2]}/>
       </Canvas>
+      <button className="neonText w-16 absolute top-4 left-4 lg:w-32 rounded-xl bg-primary px-6 py-2.5 text-sm text-white hover:bg-primary/80">Go Back</button>
     </div>
     )
 }
