05099706d2be8c9787a787ba19d891afad154504
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-11-28 10:12:46 -0600

    Removed React Spring animated Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 import { useLocation, Routes, Route } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
-//import {animated, useTransition } from "react-spring"
+
 import {
   First,
   Cards,
@@ -16,22 +16,16 @@ import {
   Picture2,
   Room,
 } from "./pages";
-//const Room = lazy(() => import("./pages/Room"));
 
 function App() {
   const location = useLocation();
-  // const transitions = useTransition(location, {
-  //   from: { opacity:0.3, filter: 'blur(100px)' ,transform: 'rotate(180deg)'},
-  //   enter: { opacity:1,filter: 'blur(0px)', transform: 'rotate(0deg)'},
-  //   leave: { opacity:0.3,filter: 'blur(100px)', transform: ' rotate(-180deg)'},
-  //   config: {duration: 750},
-  //   exitBeforeEnter: true
-  // });
+  
   console.log(
     "%cThank You for visiting my Portfolio. Please consider hiring me!",
     "font-weight: thin; color: #ff0000; font-size:3rem",
   );
   return (
+   
     <NextUIProvider>
       <Routes location={location}>
         <Route path="/" exact element={<First />} />
@@ -76,13 +70,3 @@ function App() {
 
 export default App;
 
-/*
-{transitions((props, item) => (
-  <animated.div style={props}>
-    <Routes location={item}>
-      <Route path="/" exact element={<Room />} />
-      <Route path='/placeholder' exact element={<Placeholder />} />
-    </Routes>
-  </animated.div>
-))}
-*/
