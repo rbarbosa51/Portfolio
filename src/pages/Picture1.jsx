33f873e68cdb9bf445d15a9f99c9dad5e9eae6ce
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-12-02 13:57:49 -0600

    Before Picture 1

--- src/pages/Picture1.jsx
+++ src/pages/Picture1.jsx
@@ -2,12 +2,14 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment, Sky } from "@react-three/drei";
 import { Rafael } from "../components/Rafael";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@nextui-org/react";
 
 export default function Picture1() {
   const navigate = useNavigate();
   return (
     <div className="relative h-screen">
       <Canvas>
+        <OrbitControls />
         <ambientLight intensity={1.0} />
         <Environment preset="sunset" />
         <Sky
@@ -18,12 +20,19 @@ export default function Picture1() {
         />
         <Rafael position={[0, -1, 3.2]} />
       </Canvas>
-      <button
+      {/* <button
         className="neonText absolute left-4 top-4 rounded-xl bg-primary px-6 py-2.5 text-sm text-white hover:bg-primary/80"
         onClick={() => navigate("/room")}
       >
         Go Back
-      </button>
+      </button> */}
+      <Button
+        className="neonText absolute left-1/2 -translate-x-1/2 lg:translate-x-0 lg:left-4 bottom-4 lg:top-4"
+        color="primary"
+        onClick={() => navigate("/room")}
+      >
+        Back
+      </Button>
     </div>
   );
 }
